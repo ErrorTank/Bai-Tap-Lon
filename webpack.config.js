6533209688ad7db8ab43823e451da12255a4d4d6
Tuan Anh <ncq998@gmail.com>
2019-02-01 08:53:54 +0700

    create bundle dir

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     publicPath: "/",
-    path:  path.resolve(__dirname, 'public'),
+    path:  path.resolve(__dirname, 'public/bundle'),
 
   },
   resolve: {
