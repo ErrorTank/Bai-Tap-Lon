256e6f52ab841168eeed74c947f41808aebdc62e
Tater <taterazay98@gmail.com>
2019-02-03 19:35:53 +0700

    delete old class + change to orgLocation.js (test)

--- server/db/orgLocation-sql.js
+++ server/db/orgLocation-sql.js
@@ -2,8 +2,32 @@ const {DBError} = require("../utils/error/error-types");
 const createQuery = require("../config/query");
 const isNil = require("lodash/isNil");
 
+var locationObj = {
+  name: '1',
+  address: '2'
+};
+var {name, address} = locationObj;
+
 const orgLocationSql = (db) => {
   const query = createQuery(db);
+  //create location
+  const createLocation = (locationObj) => {
+    var id = uniqid();
+    this.locationID = id.slice(-6,-1)+id.slice(-1);
+
+    var createInfo = `INSERT INTO orgLocation (orgLocationID, name, content) VALUES(${locationID}, ${name}, ${address})`;
+    return new Promise((resolve, reject) =>
+        query(createInfo).then((result) => {
+          if(result.length){
+            resolve(result[0]);
+          }else{
+            reject(new Error("Can't create location"));
+          }
+        }).catch(err => {
+          reject(err)
+        })
+      )
+  };
 
   //get location
   const getLocation = (locationID) => {
@@ -22,7 +46,7 @@ const orgLocationSql = (db) => {
   };
 
   //update location's info
-  const updateLocation = (locationID, name, address) => {
+  const updateLocation = (locationID, locationObj) => {
     var updateInfo = `UPDATE orgLocation SET name = ${name}, address = ${address} WHERE orgLocationID = ${locationID}`;
     return new Promise((resolve, reject) =>
         query(updateInfo).then((result) => {
@@ -39,13 +63,14 @@ const orgLocationSql = (db) => {
 
   //delete location
   const deleteLocation = (locationID) => {
+    
     var deleteInfo = `DELETE FROM orgLocation WHERE orgLocationID = ${locationID}`;
     return new Promise((resolve, reject) =>
         query(deleteInfo).then((result) => {
           if(result.length){
             resolve(result[0]);
           }else{
-            reject(new Error("Can't update location"));
+            reject(new Error("Can't delete location"));
           }
         }).catch(err => {
           reject(err)
@@ -54,6 +79,7 @@ const orgLocationSql = (db) => {
   }
 
   return {
+    createLocation,
     getLocation,
     updateLocation,
     deleteLocation
