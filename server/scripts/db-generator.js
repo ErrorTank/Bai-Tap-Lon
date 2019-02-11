e578054aa548a4567a9581f097fb1d0b6f0f71f9
Main <hohohovip1@gmail.com>
2019-02-11 20:32:45 +0700

    9 school total added

--- server/scripts/db-generator.js
+++ server/scripts/db-generator.js
@@ -1,7 +1,6 @@
 const initializeDb = require("../config/db");
 
-const dbUtils = [
-  {
+const dbUtils = [{
     name: "Create Account table",
     query: "CREATE TABLE `olympic`.`Account` ( `accountID` NVARCHAR(10) NOT NULL ,  `username` NVARCHAR(50) NOT NULL , `password` NVARCHAR(50) NOT NULL , `role` INT(11) NOT NULL , `canLogin` BOOLEAN NOT NULL , PRIMARY KEY (`accountID`), UNIQUE (`username`)) ENGINE = InnoDB;"
   }, {
@@ -25,19 +24,19 @@ const dbUtils = [
   }, {
     name: "Create contest - subject table",
     query: "CREATE TABLE `olympic`.`contestSubject` ( `contestID` NVARCHAR(10) NOT NULL , `subjectID` NVARCHAR(10) NOT NULL, PRIMARY KEY (`contestID`, `subjectID`)) ENGINE = InnoDB;"
-  },{
+  }, {
     name: "Create subject table",
     query: "CREATE TABLE `olympic`.`subject` ( `subjectID` NVARCHAR(10) NOT NULL , `name` NVARCHAR(50) NOT NULL , PRIMARY KEY (`subjectID`)) ENGINE = InnoDB;"
-  },{
+  }, {
     name: "Create prize table",
     query: "CREATE TABLE `olympic`.`prize` ( `prizeID` NVARCHAR(10) NOT NULL , `name` NVARCHAR(50) NOT NULL , `content` NVARCHAR(100) NULL , PRIMARY KEY (`prizeID`)) ENGINE = InnoDB;"
-  },{
+  }, {
     name: "Create organizing location table",
     query: "CREATE TABLE `olympic`.`orgLocation` ( `orgLocationID` NVARCHAR(10) NOT NULL , `name` NVARCHAR(100) NOT NULL , `address` NVARCHAR(200) NOT NULL , PRIMARY KEY (`orgLocationID`)) ENGINE = InnoDB;"
-  },{
+  }, {
     name: "Create room table",
     query: "CREATE TABLE `olympic`.`room` ( `roomID` NVARCHAR(10) NOT NULL , `orgLocationID` NVARCHAR(10) NOT NULL ,`name` NVARCHAR(50) NOT NULL , PRIMARY KEY (`roomID`)) ENGINE = InnoDB;"
-  },{
+  }, {
     name: "Create candidate table",
     query: "CREATE TABLE `olympic`.`candidate`(`cID` NVARCHAR(10) NOT NULL, `accountID` NVARCHAR(10) NOT NULL , `address` NVARCHAR(200) NOT NULL, `sID` NVARCHAR(10) NOT NULL, `name` NVARCHAR(50) NOT NULL, `phone` NVARCHAR(20) NOT NULL, `email` NVARCHAR(100) NOT NULL, `CMT` NVARCHAR(20) NOT NULL , `gender` INT(1) NOT NULL  ,  `dob` DATETIME NOT NULL, PRIMARY KEY(`cID`), UNIQUE (`accountID`), UNIQUE (`CMT`)) ENGINE = InnoDB;"
   }, {
@@ -54,7 +53,7 @@ const dbUtils = [
     query: "CREATE TABLE `olympic`.`contestCandidate`(`cID` NVARCHAR(10) NOT NULL, `contestID` NVARCHAR(10) NOT NULL, `participantID` NVARCHAR(10) not null ,`candidateID` NVARCHAR(10) NOT NULL, PRIMARY KEY(`cID`, `contestID`)) ENGINE = InnoDB;"
   }, {
     name: "Insert schools",
-    query: "INSERT INTO `school` (`sID`, `name`, `address`, `phone`, `email`) VALUES ('0', 'Đại học Thăng Long', '69 Nguyễn Xiển, Thanh Xuân, Hà Nội', '01234567', 'tlu@Gmail.com'), ('1', 'Đại học Công Đoàn', '169 Tây Sơn, Đống Đa, Hà Nội', '0212135', 'congdoan@Gmail.com'), ('2', 'Đại học Bách Khoa Hà Nội', '01 Đại Cồ Việt, Hai Bà Trưng, Hà Nội', '0212135', 'bku@Gmail.com'), ('3', 'Học viện ngân hàng', '30 Chùa bộc, Đống Đa, Hà Nội', '132133', 'banking@gmail.com'), ('4', 'Học viện công nghệ bưu chính viễn thông', '02 Quang Trung, Hà Đông, Hà Nội', '01216457464', 'ptit@edu.com')"
+    query: "INSERT INTO `school` (`sID`, `name`, `address`, `phone`, `email`) VALUES ('0', 'Đại học Thăng Long', '69 Nguyễn Xiển, Thanh Xuân, Hà Nội', '01234567', 'tlu@Gmail.com'), ('1', 'Đại học Công Đoàn', '169 Tây Sơn, Đống Đa, Hà Nội', '0212135', 'congdoan@Gmail.com'), ('2', 'Đại học Bách Khoa Hà Nội', '01 Đại Cồ Việt, Hai Bà Trưng, Hà Nội', '0212135', 'bku@Gmail.com'), ('3', 'Học viện ngân hàng', '30 Chùa bộc, Đống Đa, Hà Nội', '132133', 'banking@gmail.com'), ('4', 'Học viện công nghệ bưu chính viễn thông', '02 Quang Trung, Hà Đông, Hà Nội', '01216457464', 'ptit@edu.com'), ('5', 'Trường Đại học Kinh tế Quốc dân', '207 Giải Phóng, Đồng Tâm, Quận Hai Bà Trưng, Hà Nội', '02436280280', 'cnttkt@neu.edu.vn'), ('7', 'Trường Đại học Y Hà Nội', '01 Tôn Thất Tùng, Đống Đa, Hà Nội', '0438523798', 'daihocyhn@hmu.edu.vn'), ('8', 'Trường Đại học Giao Thông Vận Tải', '03 Cầu Giấy, Láng Thượng, Đống Đa, Hà Nội', '02437663311', 'dhgtvt@utc.edu.vn') "
   }
 
 ];
